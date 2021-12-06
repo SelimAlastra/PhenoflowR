97fa0d66f96a6696cc8b440b5415b87b727a39b5
Selim Alastra <selim.alastra@kcl.ac.uk>
2021-12-06 22:48:44 +0000

    Adds contact us page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer')
 
 mongoose.connect('mongodb://localhost/PhenoflowR', {
     useNewUrlParser: true,
@@ -56,6 +57,48 @@ app.get('/contactUs', function(req,res) {
     res.render('contactUs.pug')
 });
 
+app.post('/contactUs', function(req,res) {   
+    const output = `
+        <p> You received a new message from the Contact Us page </p>
+        <h3> Contact details </h3>
+        <ul>
+            <li> First name : ${req.body.FirstName} </li>
+            <li> Last name : ${req.body.LastName} </li>
+            <li> Email : ${req.body.Email} </li>
+        </ul>
+        <h3> Message </h3>
+        <p>${req.body.Question}</p>
+    `;
+
+    const transporter = nodemailer.createTransport( {
+        service: "hotmail",
+        auth: {
+            user: "phenoflowr@hotmail.com",
+            pass: "phenoflow12345"
+        }
+    });
+    const options = {
+        from: "phenoflowr@hotmail.com",
+        to: "phenoflowr@gmail.com",
+        subject: `New message from ${req.body.LastName}`,
+        text: "You received a new message",
+        html: output
+    }
+    
+    transporter.sendMail(options, function(err,info){
+        if(err){
+            return console.log(err);
+        }
+        else{
+        console.log("Sent:" + info.response);
+        res.render('contactUs.pug', {
+            msg: 'Your message has been sent !'
+        });
+    }   
+    });
+
+});
+
 //Set Up route
 app.get('/setUp', function(req,res) {
     res.render('setUp.pug')
@@ -81,7 +124,7 @@ app.get('/register', function(req,res) {
     res.render('register.pug', {
         title: 'Register on Phenoflow'
     }); 
-})
+});
 
 //Add Register Sumbit POST route
 app.post('/register', function(req,res) {
@@ -98,7 +141,7 @@ app.post('/register', function(req,res) {
             res.redirect('/')
         }
     });
-})
+});
 
 //Start server
 app.listen(3000, function(){
